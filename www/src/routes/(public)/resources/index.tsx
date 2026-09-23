c8346451e39192691a12c6a9573e25a61f960093
la niina <la.niina.me@gmail.com>
2026-09-24 02:19:04 +0300

    refactor: remove obsolete resource route files

--- www/src/routes/(public)/resources/index.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/(public)/resources/')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/(public)/resources/"!</div>
-}
--- www/src/routes/(public)/resources/route.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute, Outlet } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/(public)/resources')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <Outlet />
-}
